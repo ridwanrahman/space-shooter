5a5c7e6f140ad748ead6ec26ad4e91e444941598
Ridwan Rahman <ridwan_rahman@ymail.com>
2022-08-26 21:20:49 +1000

    Added ship class

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ import time
 import random
 import utils
 
+from Ship import Ship
+
+pygame.font.init()
+
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Shooter")
@@ -29,10 +33,27 @@ BG = pygame.transform.scale(pygame.image.load(os.path.join(utils.get_assets_fold
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 25)
+
+    ship = Ship(300, 650)
+
     clock = pygame.time.Clock()
 
     def redraw_window():
+        """Draws the background image and writes level
+        """
         WIN.blit(BG, (0, 0))
+        # draw text
+        lives_label = main_font.render(f"Level: {lives}", 1, (255, 255, 255))
+        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH-level_label.get_width() - 10, 10))
+
+        ship.draw(WIN)
+
         pygame.display.update()
 
     while run:
@@ -45,5 +66,8 @@ def main():
 
 
 if __name__ == '__main__':
+    """38:43 in the tutorial
+    https://www.youtube.com/watch?v=Q-__8Xw9KTM&t=876s
+    """
     main()
 
